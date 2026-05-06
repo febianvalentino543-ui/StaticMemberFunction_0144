73d4410c475ebfb8a519d6505e8dbb726fc5602f
febianvalentino543-ui <febianvalentino543@gmail.com>
2026-05-06 19:37:16 +0700

    menambahkan class mahasiswa dengan AM publik

--- staticmember.cpp
+++ staticmember.cpp
@@ -1,3 +1,18 @@
 #include <iostream>
 using namespace std;
 
+class Mahasiswa {
+
+public:
+    static int nim;
+    int id;
+    string nama;
+
+    void setID();
+    void printAll();
+
+    Mahasiswa(string pnama) :nama(pnama) {
+        setID();
+    }
+};
+
