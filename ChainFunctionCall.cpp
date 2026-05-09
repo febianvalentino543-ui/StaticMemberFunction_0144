18d0c932f90c99ef9e66fc63029494f574eacc91
febianvalentino543-ui <febianvalentino543@gmail.com>
2026-05-09 21:03:25 +0700

    membuat kelas buku dan objek lalu memanggilnya

--- ChainFunctionCall.cpp
+++ ChainFunctionCall.cpp
@@ -1,3 +1,25 @@
 #include <iostream>
 using namespace std;
 
+class buku{
+    string judul;
+
+    public:
+    buku &setJudul(string judul){
+        this->judul = judul;
+        return *this;
+    }
+    string getJudul(){
+        return this->judul;
+    }
+
+};
+
+int main()
+{
+    buku bukunya;
+    // bukunya.setJudul("Matematika");
+    // cout << bukunya.getJudul();
+    cout<<bukunya.setJudul("Matematika").getJudul(); // chaining function call
+    return 0;
+}
