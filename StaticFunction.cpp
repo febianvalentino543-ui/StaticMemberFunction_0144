11296bf9a60aab36b7dc78a68d1ee397807a5753
febianvalentino543-ui <febianvalentino543@gmail.com>
2026-05-09 20:43:27 +0700

    menambahkan kelas Mahasiswa dengan atribut dan metode statis

--- StaticFunction.cpp
+++ StaticFunction.cpp
@@ -2,3 +2,26 @@
 #include <string>
 using namespace std;
 
+class Mahasiswa {
+private:
+    static int nim;
+public:
+    int id;
+    string nama;
+
+    void setID();
+    void printAll();
+
+    static void setNim(int pnim) {
+        nim = pnim;
+    };
+
+    static int getNim() {
+        return nim;
+    }
+
+    Mahasiswa(string pnama) :nama(pnama) {
+        setID();
+    };
+};
+
