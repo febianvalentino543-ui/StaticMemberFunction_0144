faac48389d6dc23d51b0bb35e6b28ff476bd9486
febianvalentino543-ui <febianvalentino543@gmail.com>
2026-05-09 20:58:50 +0700

    menambahkan implementasi konstruktor dalam kelas mahasiswa

--- ConstructorOverloading.cpp
+++ ConstructorOverloading.cpp
@@ -14,3 +14,19 @@ class mahasiswa{
     void cetak();
 };
 
+mahasiswa :: mahasiswa(){
+}
+
+mahasiswa :: mahasiswa(int iNim){
+    nim = iNim; // definisi hanya untuk NIM
+}
+
+mahasiswa :: mahasiswa(string iNama){
+    nama = iNama; // definisi hanya untuk NAMA
+}
+
+mahasiswa :: mahasiswa(int iNim, string iNama){
+    nim = iNim; // definisi untuk NIM dan NAMA
+    nama = iNama;
+}
+
